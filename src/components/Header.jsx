48f58da6abe92a8df856408923ac11666b5a6a0d
Kachii <kachikaduru@gmail.com>
2024-08-19 21:37:23 +0100

    New PortFolio Website!

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import Logo from "./Logo";
+import Navbar from "./Navbar";
+
+export default function Header() {
+  return (
+    <header>
+      <Logo />
+
+      <Navbar />
+    </header>
+  );
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,38 @@
+export default function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Tech Stack</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
+      </ul>
+
+      <ul>
+        <li>
+          <a href="#">
+            <img src="" alt="GitHub" />
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <img src="" alt="Twitter" />
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <img src="" alt="LinkedIn" />
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
